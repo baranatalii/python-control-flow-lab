d963852386d3aecc2115c2992b7234f6add5317c
Baranatalii <baranatalii@BARANs-MacBook-Air.local>
2026-02-20 11:34:18 -0500

    Exercise 0 Example

---
+++ exercises.py
@@ -0,0 +1,12 @@
+# Exercise 0: Example
+#
+# This is a practice exercise to help you understand how to write code "inside" a provided Python function.
+
+def print_greeting():
+    # Your code goes here. Remember to indent!
+    python_is_fun = True
+    if python_is_fun:
+        print("Python is fun!")
+
+# Call the function
+print_greeting()
